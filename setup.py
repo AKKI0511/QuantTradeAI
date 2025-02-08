a922b1c6c2887e3bdc92fbb65cbcebd43675864c
Akshat <akkijoshi0511@gmail.com>
2025-02-07 23:52:43 -0500

    Requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quanttradeai",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "yfinance",
+        "scikit-learn",
+        "xgboost",
+        "pandas-ta",
+        "matplotlib",
+        "seaborn",
+    ],
+    author="Akshat Joshi",
+    author_email="joshiakshat0511@gmail.com",
+    description="A machine learning framework for quantitative trading strategies",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+) 
